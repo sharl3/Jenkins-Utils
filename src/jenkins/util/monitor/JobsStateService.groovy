eefc5a854b8f6fad3e7a4bf6617dca74f43fd11a
sharl3 <arthur.resse@gmail.com>
2017-03-09 23:17:59 +0100

    Finalized shit

--- src/jenkins/util/monitor/JobsStateService.groovy
+++ src/jenkins/util/monitor/JobsStateService.groovy
@@ -1,26 +1,30 @@
-import hudson.model.*
-import hudson.node_monitors.*
-import hudson.slaves.*
-import java.util.concurrent.*
+ 
 jenkins = Hudson.instance
-// Define hour to compare (hour=24 will find builds that were built more than 1 day ago)
-hour=24;
-minute=60;
-second=60;
-oneDayInSecond=hour*minute*second;
 now=Calendar.instance;
-list=[];
+
 
 println("The build run at ${now.time}");
-println("Job's name;Disabled;Last build date;Last build status")
+println("Job's name;Disabled;Last execution date;Last build status;Evolution")
+
+List<List<String>> jobsStatuses = new ArrayList<List<String>>();
 for (item in jenkins.items){
-  String text = "${item.name};${item.disabled};";
-   if (item.getLastBuild()!=null){
-	  lastBuild = item.getLastBuild();
-	  String lastExecution = lastBuild.getTimestamp().getTime().format("yyyy-MM-dd");
-	  String evolution = lastBuild.getBuildStatusSummary().message;
-	  String result = lastBuild.getResult().toString();
-	  text = "${text}${lastExecution};${result};${evolution}";
+	List<String> text = new ArrayList<String>();
+	text.add(item.name);
+	text.add(item.disabled);
+	lastBuild = item.getLastBuild();
+   if (lastBuild!=null){
+	  // lastExecution #
+	  text.add(lastBuild.getTimestamp().getTime().format("yyyy-MM-dd"));
+	  // result 
+	  text.add(lastBuild.getResult().toString());
+	  // evolution 
+	  text.add(lastBuild.getBuildStatusSummary().message);
 	}
-	println(text);
+	jobsStatuses.add(text);
+}
+jobsStatuses.each {
+	it.each {
+		print("${it};")
+	}
+	println();
 }
