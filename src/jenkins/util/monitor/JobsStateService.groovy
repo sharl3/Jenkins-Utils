02857b0502dfd11281cbbf6e4fe260573f201d9f
sharl3 <arthur.resse@gmail.com>
2017-03-09 23:19:15 +0100

    ^^

--- src/jenkins/util/monitor/JobsStateService.groovy
+++ src/jenkins/util/monitor/JobsStateService.groovy
@@ -7,20 +7,21 @@ println("The build run at ${now.time}");
 println("Job's name;Disabled;Last execution date;Last build status;Evolution")
 
 List<List<String>> jobsStatuses = new ArrayList<List<String>>();
-for (item in jenkins.items){
-	List<String> text = new ArrayList<String>();
-	text.add(item.name);
-	text.add(item.disabled);
-	lastBuild = item.getLastBuild();
+
+for (job in jenkins.items){
+	List<String> jobStatuts = new ArrayList<String>();
+	jobStatuts.add(job.name);
+	jobStatuts.add(job.disabled);
+	lastBuild = job.getLastBuild();
    if (lastBuild!=null){
 	  // lastExecution #
-	  text.add(lastBuild.getTimestamp().getTime().format("yyyy-MM-dd"));
+	  jobStatuts.add(lastBuild.getTimestamp().getTime().format("yyyy-MM-dd"));
 	  // result 
-	  text.add(lastBuild.getResult().toString());
+	  jobStatuts.add(lastBuild.getResult().toString());
 	  // evolution 
-	  text.add(lastBuild.getBuildStatusSummary().message);
+	  jobStatuts.add(lastBuild.getBuildStatusSummary().message);
 	}
-	jobsStatuses.add(text);
+	jobsStatuses.add(jobStatuts);
 }
 jobsStatuses.each {
 	it.each {
